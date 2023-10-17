0d342c6891a080ea608d516f0a36e1634167d7df
Mladen Trišić <mtrisic@gmail.com>
2023-10-17 16:35:51 +0200

    Action.yml syntax fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Building some OpenAPI specs for you..."
+echo "START - Building some OpenAPI specs..."
 vertag=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 30)
 outputUrls=""
 
@@ -29,6 +29,7 @@ do
   outfname=${GITHUB_WORKSPACE}/${i}_${vertag}.html
   fname=$(basename $outfname)
   npx redoc-cli bundle $GITHUB_WORKSPACE/$i -o $outfname --disableGoogleFont --options.expandDefaultServerVariables "true" 
+  echo "Copying $outfname to s3://$BUCKET_NAME/$fname"
   aws --endpoint-url $aws_endpoint s3 cp $outfname s3://$BUCKET_NAME/
   aws --endpoint-url $aws_endpoint s3api put-object-acl --bucket $BUCKET_NAME --acl public-read --key $fname
   out="== Uploaded spec successfully to https://${BUCKET_NAME}.${AWS_ENDPOINT}/$fname =="
